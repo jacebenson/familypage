e03e29d3052f9e9d8482f9bf98f3a77cc355f594
Jace <638764+jacebenson@users.noreply.github.com>
2023-08-28 22:29:52 -0500

    oh no

--- web/src/components/Card/Card.jsx
+++ web/src/components/Card/Card.jsx
@@ -29,7 +29,6 @@ const Card = ({
   if (!features) { features = ['feature 1', 'feature 2', 'feature 3'] }
   if (!CTAText) { CTAText = 'Learn more' }
   if (!CTALink) { CTALink = 'https://ddg.gg' }
-  if (!CTATarget) { CTATarget = '_self' }
   return (
     <Center py={2}>
       <Box
@@ -84,7 +83,6 @@ const Card = ({
           <Button
             as={'a'}
             href={CTALink}
-            target={CTATarget || '_self'}
             mt={10}
             w={'full'}
             bg={'green.400'}

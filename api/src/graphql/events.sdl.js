ee0346b0906731f4f329915d0aaf003a1d27e959
Jace <638764+jacebenson@users.noreply.github.com>
2023-09-10 00:20:29 -0500

    reverted to 6.1.0

--- api/src/graphql/events.sdl.js
+++ api/src/graphql/events.sdl.js
@@ -73,7 +73,6 @@ export const schema = gql`
     createEvent(input: CreateEventInput!): Event! @requireAuth
     createEventWithAttendees(input: CreateEventWithAttendeesInput!): Event! @requireAuth
     updateEvent(id: String!, input: UpdateEventInput!): Event! @requireAuth
-    updateEventPublic(id: String!, input: UpdateEventInput!): Event! @requireAuth
     deleteEvent(id: String!): Event! @requireAuth
   }
 `
